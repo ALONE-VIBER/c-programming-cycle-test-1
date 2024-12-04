38e0793d34932c485377d0ecea87de0c314d81b3
BALASURYA <kuttypulisurya22@gmail.com>
2024-12-04 20:07:41 +0530

    created an code for question 1

---
+++ q1.c
@@ -0,0 +1,14 @@
+/*
+Write a C program to compute the sum of the first 10 natural numbers
+*/
+#include<stdio.h>
+
+void main(){
+
+    int sum = 0 ;
+    for(int natural = 1; natural < 11; ++natural){
+        sum += natural;
+    } 
+    printf("sum of the first 10 natural numbers : %d\n",sum);
+ 
+}
