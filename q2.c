599917e370279dd9c3680c43272d6023d5059d53
BALASURYA <kuttypulisurya22@gmail.com>
2024-12-04 22:31:51 +0530

    combined files

--- q2.c
+++ q2.c
@@ -3,6 +3,19 @@ Execute the linux commands to open a file and Write a C program to compute
 whether a given integer is positive even, negative even, positive odd or
 negative odd.. Print even if the number is 0. 
 */
+/*
+step 1 : create an file named 'q2_b.c' 
+command: touch q2_b.c
+
+step 2: edit the file 'q2_b.c' on text editor 
+
+step 3: compile the c file
+command: gcc q2_b.c -o q2_output
+
+step 4: executing the compiled file
+command: ./q2_output
+*/
+
 #include<stdio.h>
 
 void main(){
--- q6.c
+++ q6.c
@@ -2,6 +2,21 @@
 Execute the linux commands to open a file and Write a C program that prints
 all even numbers between 1 and 50 (inclusive).
 */
+
+/*
+step 1 : create an file named 'q6_b.c' 
+command: touch q6_b.c
+
+step 2: edit the file 'q6_b.c' on text editor 
+
+step 3: compile the c file
+command: gcc q6_b.c -o q6_output
+
+step 4: executing the compiled file
+command: ./q6_output
+*/
+
+
 #include<stdio.h>
 
 void main(){
