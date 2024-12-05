43ddc204d627926548918d7837b886065f05ef5b
BALASURYA <kuttypulisurya22@gmail.com>
2024-12-05 20:28:25 +0530

    descrprion

--- q1.c
+++ q1.c
@@ -7,7 +7,7 @@ void main(){
 
     int sum = 0 ;
     for(int natural = 1; natural < 11; ++natural){
-        sum += natural;
+        sum += natural;//sum = sum + natural
     } 
     printf("sum of the first 10 natural numbers : %d\n",sum);
  
